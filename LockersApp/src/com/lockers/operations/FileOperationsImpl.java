4ba701650939db71f1fd50645879f6a38c320f87
abhi-ghute <abghute7@gmail.com>
2022-08-21 22:47:31 +0530

    DeleteFile and search file

--- LockersApp/src/com/lockers/operations/FileOperationsImpl.java
+++ LockersApp/src/com/lockers/operations/FileOperationsImpl.java
@@ -9,70 +9,80 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class FileOperationsImpl implements FileOperations {
-	
+
 	@Override
 	public void getFileNames(String directoryPath) {
-		
+
 		String[] fNames = null;
 		File file = new File(directoryPath);
-		
+
 		try {
-			fNames = file.list(); 
+			fNames = file.list();
 			if (fNames == null) {
 				throw new NullPointerException();
 			}
 		} catch (Exception npe) {
 			System.out.println("Specified Folder is not present in your system... want to create(y/n)");
-			
+
 			Scanner sc = new Scanner(System.in);
 			char choice = sc.next().toLowerCase().charAt(0);
 			sc.close();
-			
-			if(choice=='y') {
+
+			if (choice == 'y') {
 				file.mkdir();
-				fNames = file.list(); 
-			}
-			else {
+				fNames = file.list();
+			} else {
 				System.out.println("Please specify another directory path");
 				return;
 			}
-			
+
 		}
-		
-		
-		if (fNames.length==0) {
+
+		if (fNames.length == 0) {
 			System.out.println("No file present inside the folder..");
 		}
-		
+
 		Arrays.sort(fNames);
-		for(int i=0;i<fNames.length;i++)
+		for (int i = 0; i < fNames.length; i++)
 			System.out.println(fNames[i]);
 	}
 
 	@Override
-	public void addFile(String directoryPath,String fileName) {
-		
-		 Path path = Paths.get(directoryPath,fileName);
-		 
-		 try {
-	         Path createdFilePath = Files.createFile(path);
-	         System.out.println("Created a file at : "+createdFilePath);
-	      } 
-	      catch (IOException e) {
-	         e.printStackTrace();
-	      }
-		
+	public void addFile(String directoryPath, String fileName) {
+
+		Path path = Paths.get(directoryPath, fileName);
+
+		try {
+			Path createdFilePath = Files.createFile(path);
+			System.out.println("Created a file at : " + createdFilePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
-	public void deleteFile(String directoryPath,String fileName) {
-		// TODO Auto-generated method stub
-		
+	public void deleteFile(String directoryPath, String fileName) {
+
+		Path path = Paths.get(directoryPath, fileName);
+
+		try {
+			Files.delete(path);
+			System.out.println("File Deleted from the Directory" + path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
-	public void searchFile(String directoryPath,String fileName) {
-		// TODO Auto-generated method stub
-		
+	public void searchFile(String directoryPath, String fileName) {
+		Path path = Paths.get(directoryPath, fileName);
+		try {
+			boolean flag = Files.exists(path);
+			System.out.println(flag);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
